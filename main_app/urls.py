f02ae9cb6d74a42ad74d52a00b37144df79ae443
Ryan Kim <hanjun1@ualberta.ca>
2021-04-04 00:43:08 -0600

    added signup sheet, fixed models, added general routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('results/', views.results, name="results"),
-    path('owners/', views.owners_home, name="owners_home"),
+    path('show_all/', views.show_all, name='show_all'),
+    path('owners/<int:owner_id>/', views.owners_home, name="owners_home"),
+    path('<int:eater_id>/favourites/', views.favourites, name="favourites"),
     path('owners/new', views.owners_new, name="owners_new"),
-    # path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, get_user_model
+# from django.contrib.auth.forms import UserCreationForm
+from main_app.forms import CustomUserCreationForm
+from django.contrib.auth import get_user_model
+from .models import Truck
+
+User = get_user_model()
 
 
 def home(request):
@@ -14,9 +21,42 @@ def results(request):
     return render(request, 'results/index.html', {'message': message})
 
 
-def owners_home(request):
-    return render(request, 'owners/index.html')
+def owners_home(request, owner_id):
+    owner = User.objects.get(id=owner_id)
+    trucks = Truck.objects.all().filter(user=owner)
+    # if owner.type == 'Owner':
+    return render(request, 'owners/index.html', {'trucks': trucks})
 
 
 def owners_new(request):
     return render(request, 'owners/new.html')
+
+
+def favourites(request, eater_id):
+    eater = User.objects.get(id=eater_id)
+    # if eater.type == 'Eater':
+    return render(request, 'users/favourites.html', {'eater': eater})
+
+
+def show_all(request):
+    return render(request, 'show.html')
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = CustomUserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
