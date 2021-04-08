b96b6aad45986b0eae340bcd3d7373d51a8414d9
Ryan Kim <hanjun1@ualberta.ca>
2021-04-07 19:07:53 -0600

    added styling to results index and show pages

--- main_app/static/javascript/navbar.js
+++ main_app/static/javascript/navbar.js
@@ -6,10 +6,10 @@ closeNavEl.addEventListener("click", closeNav);
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "300px";
-  document.querySelector(".sub-main").addEventListener("click", closeNav);
+  // document.querySelector(".sub-main").addEventListener("click", closeNav);
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.querySelector(".sub-main").removeEventListener("click", closeNav);
+  // document.querySelector(".sub-main").removeEventListener("click", closeNav);
 }
